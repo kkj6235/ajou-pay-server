07c18e4c46e1450a2d2eeda14783ec57c3876dfe
SeungHun Han <shhan730@gmail.com>
2023-11-25 19:40:37 +0900

    feat: /shop

--- api/shop/index.js
+++ api/shop/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./shop.controller');
 
-router.get('/', controller.test);
+router.get('/', controller.getShop);
 
 module.exports = router;
--- api/shop/shop.controller.js
+++ api/shop/shop.controller.js
@@ -1,5 +1,13 @@
-const test = (req, res) => {
-    res.status(200).json({ test: 'shop입니다!' });
+var mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+const getShop = async (req, res) => {
+    try {
+        const stores = await Store.find({});
+        res.json(stores);
+    } catch (error) {
+        res.status(500).send({ message: '서버 오류가 발생했습니다.' });
+    }
 };
 
-module.exports = { test };
+module.exports = { getShop };
