efb5dab3753af57f850107123d633f058c7ede46
SeungHun Han <shhan730@gmail.com>
2023-11-27 02:12:47 +0900

    feat: /shop/:shopId/menu

--- api/shop/index.js
+++ api/shop/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('./shop.controller');
 
 router.get('/', controller.getShop);
+router.get('/:shopId/menu', controller.getShopMenu);
 
 module.exports = router;
--- api/shop/shop.controller.js
+++ api/shop/shop.controller.js
@@ -1,8 +1,12 @@
 var mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const Menu = mongoose.model('Menu');
 
 const getShop = async (req, res) => {
     try {
+        if (!req.session.user) {
+            res.status(403).send({ message: '인증오류' });
+        }
         const stores = await Store.find({});
         res.json(stores);
     } catch (error) {
@@ -10,4 +14,30 @@ const getShop = async (req, res) => {
     }
 };
 
-module.exports = { getShop };
+const getShopMenu = async (req, res) => {
+    try {
+        // Check if the user is authenticated
+        if (!req.session.user) {
+            return res.status(403).json({ message: '인증오류' });
+        }
+
+        // Retrieve the shopId from the request parameters and convert it to an integer
+        const shopId = parseInt(req.params.shopId);
+
+        // Validate shopId (check if it's a valid number)
+        if (isNaN(shopId)) {
+            return res.status(400).json({ message: 'Invalid shop ID' });
+        }
+
+        // Find menu items that belong to the shop
+        const menuItems = await Menu.find({ shopId: shopId });
+
+        // Respond with the menu items
+        res.json(menuItems);
+    } catch (error) {
+        // Handle any errors that occur during the process
+        res.status(500).send({ message: '서버 오류가 발생했습니다.' });
+    }
+};
+
+module.exports = { getShop, getShopMenu };
