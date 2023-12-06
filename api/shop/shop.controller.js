812d0f08c2aa78972d5b3fb19d2c892682f3a5d6
SeungHun Han <shhan730@gmail.com>
2023-12-06 20:55:10 +0900

    style: shop.controller

--- api/shop/shop.controller.js
+++ api/shop/shop.controller.js
@@ -16,32 +16,24 @@ const getShop = async (req, res) => {
 
 const getShopMenu = async (req, res) => {
     try {
-        // Check if the user is authenticated
         if (!req.session.user) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
 
-        // Retrieve the shopId from the request parameters and convert it to an integer
         const shopId = parseInt(req.params.shopId);
 
-        // Validate shopId (check if it's a valid number)
         if (isNaN(shopId)) {
             return res.status(400).json({ message: 'Invalid shop ID' });
         }
 
-        // Find menu items that belong to the shop
         const menuItems = await Menu.find({ shopId: shopId });
-
-        // FInd shop item about the information of the shop
         const shopItem = await Store.findOne({ _id: shopId });
 
-        // Respond with the menu items
         return res.json({
             shop: shopItem,
-            menus: menuItems
+            menus: menuItems,
         });
     } catch (error) {
-        // Handle any errors that occur during the process
         return res.status(500).send({ message: '서버 오류가 발생했습니다.' });
     }
 };
