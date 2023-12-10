13dcc517b8efd70d245df2aae5e128cd74420427
MinseoLee <mmmm@ajou.ac.kr>
2023-12-10 16:42:14 +0900

    fix: client url for redirect

--- common/constants.js
+++ common/constants.js
@@ -1,14 +1,16 @@
-const DEV_CLIENT_URL = 'http://localhost:3000';
-const PROD_CLIENT_URL = 'https://';
-const CLIENT_URL = process.env.NODE_ENV === 'production' ? PROD_CLIENT_URL : DEV_CLIENT_URL;
+// const DEV_CLIENT_URL = 'http://localhost:3000';
+const PROD_CLIENT_URL = 'https://ajou-order.vercel.app';
+// const CLIENT_URL =
+//     process.env.NODE_ENV === 'production' ? PROD_CLIENT_URL : DEV_CLIENT_URL;
+const CLIENT_URL = PROD_CLIENT_URL;
 
 const CLIENT_ROUTE = {
     SUCCESS: '/order/success',
     FAIL: '/order/fail',
-    CANCEL: '/order/cancel'
-}
+    CANCEL: '/order/cancel',
+};
 
 module.exports = {
     CLIENT_URL,
-    CLIENT_ROUTE
-}
+    CLIENT_ROUTE,
+};
